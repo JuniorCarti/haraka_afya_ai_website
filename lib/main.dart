aa2151706a8e4a8408afe6227dbcd8629f2ca103
JuniorCarti <ridgejunior204@gmail.com>
2025-08-12 15:01:12 +0300

    feat: initialize main app with timeDilation for smoother demo animations

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,9 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  // Remove this in production - just for demo animations
+  timeDilation = 1.5;
+  runApp(const HarakaAfyaWebsite());
+}
